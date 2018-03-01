Fixed the softmax bug; I wasnt computing the gradients correctly; additionally, abstracted the backprop into NNUnit() class to avoid code repetition

nn.py
@@ -18,6 +18,11 @@ class NNUnit(object):
 		self.inp_size = inp_size
 		self.out_size = out_size
 
+	def clear_history(self):
+		self.lastOutput = None
+		self.lastInput = None
+		self.lastGrad = None
+
 	def forwardprop(self):
 		self.print_forwardprop()
 		if self.childUnit is not None:
@@ -25,17 +30,18 @@ class NNUnit(object):
 		else: 
 			return self.lastOutput
 
-	def clear_history(self):
-		self.lastOutput = None
-		self.lastInput = None
-		self.lastGrad = None
-
 	def print_forwardprop(self):
 		if DEBUG_MODE:
 			print "Forward:", self.name, "\t", self.lastInput, "\t", self.lastOutput
 			print self.weights if self.hasParams else None
 			print "................................................."
 
+	def backprop(self, grads):
+		self.lastGrad = grads
+		self.print_backprop()
+		newGrad = self.d_input(grads)
+		if self.parentUnit is not None:
+			self.parentUnit.backprop(newGrad)
 
 	def print_backprop(self):
 		if DEBUG_MODE:
@@ -57,7 +63,6 @@ class Linear(NNUnit):
 		elif mode=="random":
 			self.weights = np.random.normal(size=dim)
 
-
 	def forward(self, input):
 		output = np.matmul(input, self.weights)
 		self.lastOutput = output
@@ -70,16 +75,9 @@ class Linear(NNUnit):
 		weight_grads = np.matmul(input.T, grad)
 		self.weights = self.weights - learning_rate * weight_grads
 
-	def d_input(self):
-		return self.weights.T
-
-	def backprop(self, grads):
-		# This code is almost rewritten. Abstract it?
-		self.lastGrad = grads
-		self.print_backprop()
-		newGrad = np.matmul(grads, self.d_input())
-		if self.parentUnit is not None:
-			self.parentUnit.backprop(newGrad)
+	def d_input(self, grads):
+		chain = self.weights.T
+		return np.matmul(grads, chain)
 
 class Relu(NNUnit):
 	def __init__(self):
@@ -94,16 +92,10 @@ class Relu(NNUnit):
 		self.lastOutput = output
 		return self.forwardprop()
 
-	def d_input(self):
+	def d_input(self, grads):
 		input = self.lastInput
-		return np.array(input > 0).astype(np.float32)
-
-	def backprop(self, grads):
-		self.lastGrad = grads
-		self.print_backprop()
-		newGrad = grads * self.d_input()
-		if self.parentUnit is not None:
-			self.parentUnit.backprop(newGrad)
+		chain = np.array(input > 0).astype(np.float32)
+		return grads * chain
 
 
 # Implementation of below is paused until Relu is implemented correctly
@@ -117,32 +109,31 @@ class Softmax(NNUnit):
 		# To prevent softmax explosions
 		expLayer = np.power(np.e, cappedLL)
 		output = expLayer / np.sum(expLayer, axis=1)[:, np.newaxis]
-
-		# print "Softmax"
-		# print input
-		# print cappedLL
-		# print "exp", expLayer
-		# print output
-		# print 
-
 		self.lastInput = input
 		self.lastOutput = output
 		return self.forwardprop()
 
 
-	def d_input(self):
+	def d_input(self, grads):
 		"""
+		TODO: write unit tests
 		Uses the property that dSoftmax(x)/dx = Softmax(x)(1-Softmax(x))
 		"""
 		out = self.lastOutput
-		return (out*(1-out))
+		s0, s1 = out.shape
+		out1 = out.reshape((s0, s1, 1))
+		out2 = -out.reshape((s0, 1, s1))
 
-	def backprop(self, grads):
-		self.lastGrad = grads
-		self.print_backprop()
-		newGrad = grads * self.d_input()
-		if self.parentUnit is not None:
-			self.parentUnit.backprop(newGrad)
+		jacobian = np.matmul(out1, out2)
+
+		diagonal = np.eye(s1)
+		diagonal = np.repeat(diagonal[np.newaxis,:,:], s0, axis=0)
+		diagonal = diagonal * out1
+		
+		jacobian += diagonal
+
+		newGrad = np.matmul(grads.reshape((s0, 1, s1)), jacobian).reshape((s0, s1))
+		return newGrad
 
 
 class CrossEntropyLoss():

tests.py
@@ -4,19 +4,20 @@ import nn
 from graph import FeedForward
 import numpy as np
 
+inp_size = 2
+fc1_size = 3
+fc2_size = 6
+out_size = 10
+
+arch = [nn.Linear, nn.Relu, nn.Linear, nn.Relu, nn.Linear, nn.Softmax]
+dims = [(inp_size, fc1_size), None, (fc1_size, fc2_size), None, (fc2_size, out_size), None]
+lossF = nn.CrossEntropyLoss
+learning_rate = 0.05
+
 def ____hrule():
 	print "\n **********************************************************\n"
 
 def test1():
-	inp_size = 2
-	fc1_size = 3
-	fc2_size = 6
-	out_size = 10
-
-	arch = [nn.Linear, nn.Relu, nn.Linear, nn.Relu, nn.Linear, nn.Softmax]
-	dims = [(inp_size, fc1_size), None, (fc1_size, fc2_size), None, (fc2_size, out_size), None]
-	lossF = nn.CrossEntropyLoss
-	learning_rate = 0.05
 	ff = FeedForward(arch, dims, lossF, learning_rate)
 	ff.initialize_weights("uniform")
 
@@ -43,5 +44,28 @@ def test1():
 
 	ff.clear_history()
 
+def test_dSoftmax():
+	sm = nn.Softmax()
+	output = 0.1*np.array([0, 1, 2, 1, 2, 3]).reshape((2,3))
+	sm.lastOutput = output
+	grads = np.ones(6).reshape((2,3))
+	newGrad = sm.d_input(grads)
+	"""
+	Expected Jacobian: (2x3x3)
+		[ [0, 0, 0], [0, 0.09, -0.02], [0, -0.02, 0.16] ]
+		[ [0.09, -0.02, -0.03], [-0.02, 0.16, -0.06], [-0.03, -0.06, 0.21]]
 
-test1()
+	Expected newGrad: (2x3)
+		[ [0, 0.07, 0.14], 
+		  [0.04, 0.08, 0.12] ]
+	"""
+
+
+
+
+#test1()
+test_dSoftmax()
+
+
+
+